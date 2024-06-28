3cbade8a333b2f9d512047d9bd00995015e63a51
Shawon143 <sishawon07@gmail.com>
2024-06-29 02:04:50 +0600

    bad ten

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
